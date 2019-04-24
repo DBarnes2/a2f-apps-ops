163d9398350a81bbf05f0dbe841f5d25a94039e4
Daniel Barnes <dbarnes2@cs.washington.edu>
2019-04-24 12:30:45 -0700

    Stashing Carrot Testing

--- src/main/java/HelloAppEngine.java
+++ src/main/java/HelloAppEngine.java
@@ -15,11 +15,15 @@ public class HelloAppEngine extends HttpServlet {
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) 
       throws IOException {
+	  
+//	  CarrotEngine engine = new CarrotEngine(new Configuration.Builder()
+//			    .setResourceLocator(new FileResourceLocator.Builder("path/to/templates"))
+//			    .build());
 
     response.setContentType("text/plain");
     response.setCharacterEncoding("UTF-8");
 
-    response.getWriter().print("Hello App Engine!\r\n");
+    response.getWriter().print("Testing!\r\n");
 
   }
 }
